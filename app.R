2fc6ef8410eab05876b3a573ca880d03cd4643a3
NickPTaylor <nick.monkey@googlemail.com>
2016-01-07 15:31:31 +0000

    Change layout to bootstrap type
    * Changed layout of user interface
    * Tweaks to logic
    * Tidy up of code

--- app.R
+++ app.R
@@ -4,91 +4,106 @@ library(leaflet)
 library(shiny)
 
 # load data --------------------------------------------------------------------
+all_data <- read_csv("data.csv") %>% select(-c(Icon)) %>% 
+  rename(lat = Lat, lng = Long, stage = Stage) %>% 
+  mutate(log_scale = 
+         ifelse(is.na(MW), 0, 1000 * -1000 * log(1 - MW/sum(MW, na.rm = T)))
+  )
+oper_data <- all_data %>% 
+  filter(stage == 'Operational')
 
-wind_data <- read_csv('data.csv') %>% 
-  select(-c(Icon)) %>% 
-  rename(latitude = Lat, longitude = Long, stage = Stage) %>% 
-  mutate(log_scale = 5000 * log(MW + 1))
+# set up color palettes for legend ---------------------------------------------
+mw_palette <- colorNumeric(palette = "BuGn", domain = all_data$MW, n = 5)
+stage_palette <- colorFactor(palette = c("black", "red", "green", "blue"), 
+                             domain = as.factor(all_data$stage))
+
+# generate labels for markers
+create_label <- function(name, stage, mw) {
+  lab <- paste(sep = "<br/>",
+               paste("<b>", name, "</b>"),
+               paste("Status:", stage),
+               ifelse(is.na(mw), "", paste("Output:", mw)))
+}
+
 # user interface ---------------------------------------------------------------
-
-ui <- fluidPage(
-  headerPanel("UK Wind Power"),
-  sidebarPanel(
-    sliderInput(inputId = 'lvls', label = "Number of Quantile Levels",
-                value = 4, min = 2, max = 10),
-    sliderInput(inputId = 'rng',
-                label = "Select Range of Power Output to Display", 
-                dragRange = TRUE, min = 0, max = 350, value = c(0, 350)),
-    checkboxInput(inputId = 'scl', 
-                  label = "Scale Operational Plant Markers by Power Output",
-                  value = TRUE),
-    checkboxInput(inputId = 'nop', 
-                  label = "Display Non-Operational Plant",
-                  value = TRUE), 
-    checkboxGroupInput(inputId = 'lyrs', "Display Layers",
-                       choices = c("Construction","Planning", "Consented"))
-  ),
-  mainPanel(
-    leafletOutput(outputId = "map", height = 600)
+ui <- bootstrapPage (
+  tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+  leafletOutput("map", width = "100%", height = "100%"),
+  absolutePanel(
+    top = 100, left = 10,
+    
+    sliderInput(
+      inputId = "rng", 
+      label = "Select Range of Power Output to Display", 
+      min = 0, max = max(oper_data$MW), value = c(0, max(oper_data$MW))
+    ), 
+    checkboxInput(
+      inputId = "scl",
+      label = "Scale Operational Plant Markers by Power Output" 
+    ),
+    checkboxGroupInput(
+      inputId = "lyrs", label = "Display Layers",
+      choices = levels(as.factor(all_data$stage)), 
+      selected = levels(as.factor(all_data$stage))
+    )
   )
 )
 
 # server -----------------------------------------------------------------------
-
-server <- function(input, output){
-    
-    mw_palette <- colorQuantile(palette = c('BuGn'), 
-                                    domain = wind_data$MW, n = 5)
-    stage_palette <- colorFactor(palette = c('black', 'red', 'green', 'blue'), 
-                                 domain = as.factor(wind_data$stage))
-    
-    filter_data <- reactive({
-       wind_data %>% filter(is.na(MW) | (MW > input$rng[1] & MW < input$rng[2]))
-    })
-     
-    
-    output$map <- renderLeaflet({
-      leaflet(filter_data()) %>%
-        addProviderTiles('Stamen.TonerLite', group = 'TonerLite') %>%
-        fitBounds(-10, 50, 0, 60) %>%
-        addCircles(data = filter(filter_data(), stage == 'Operational'), radius = ~log_scale,
-                   fill = TRUE, stroke = FALSE, color = ~mw_palette(MW), 
-                   fillOpacity = 0.5,
-                   group = 'operational_scaled') %>%
-        addCircles(group = 'non_operational',
-                   popup =  ~ifelse(is.na(MW), name, paste(name, "\n", MW, "MW")),
-                   fillOpacity = 1, radius = 1, col = ~stage_palette(stage))  
-    })
-
-    observe({
-      proxy <- leafletProxy('map', data = filter_data()) 
-     
-      if(input$nop) {
-        proxy %>% 
-          showGroup('non_operational') %>% 
-          addLegend('topleft', pal = stage_palette, values = ~stage, 
-                    title = 'Stage of Development', layerId = 'non_operational')    
-     } else {
-        proxy %>% 
-          hideGroup('non_operational') %>% 
-          removeControl('non_operational')
-     } 
-   })
+server <- function(input, output, session) {
   
-   observe({
-     proxy <- leafletProxy('map', data = filter_data())
-     
-     if(input$scl){
-       proxy %>% 
-         showGroup('operational_scaled') %>% 
-         addLegend('bottomleft', pal = mw_palette, values = ~MW, 
-                title = 'Output in MW', layerId = 'operational_scaled')  
-     } else {
-       proxy %>% 
-         hideGroup('operational_scaled') %>% 
-         removeControl('operational_scaled')
-     }
-   }) 
+  # reactive expression for filtering data -------------------------------------
+  filter_data <- reactive({
+    all_data %>% 
+      filter(stage %in% input$lyrs) %>% 
+      filter(is.na(MW) | (MW > input$rng[1] & MW < input$rng[2]))
+    })
+  
+  # base map output ------------------------------------------------------------
+  output$map <- renderLeaflet({
+    leaflet(all_data) %>% 
+      addTiles() %>%  
+      setView(lng = -4.3, lat = 55, zoom = 6)  
+  })
+
+  # react to filtering of markers for all data ---------------------------------
+  observe({
+    
+    # map proxy ----------------------------------------------------------------
+    proxy <- leafletProxy("map", data = filter_data()) %>% 
+       clearShapes() %>% 
+       clearGroup("oper")
+    
+    # update filtered base points ---------------------------------------------- 
+    proxy %>%   
+      addCircles(popup = ~create_label(name, stage, MW),
+                 fillOpacity = 1, radius = 1, col = ~stage_palette(stage),
+                 group = "all_stage") %>% 
+      addLegend("bottomleft", pal = stage_palette, values = ~stage, 
+                title = "Stage of Development", layerId = "stg")
+   
+    # toggle scaled operational points ----------------------------------------- 
+    if (input$scl & 'Operational' %in% input$lyrs){
+      proxy %>% 
+        addCircles(data = filter_data() %>% filter(stage == 'Operational'),
+                   radius = ~log_scale, fillOpacity = 0.5, 
+                   popup = ~create_label(name, stage, MW),  
+                   fillColor = ~mw_palette(MW),weight = 1, col = "green",
+                   group = "oper") %>% 
+        addLegend("bottomright", pal = mw_palette, values = ~oper_data$MW, 
+                title = "Ouput (MW)", 
+                layerId = "oper")
+    } else {
+      proxy %>%
+        removeControl("oper") %>% 
+        clearGroup("oper")
+    } 
+  })
+  
+#   # plot output --------------------------------------------------------------
+#   output$plot <- renderPlot({
+#     hist(filter_data()$MW) 
+#   })
 }
 
 shinyApp(ui = ui, server = server)
